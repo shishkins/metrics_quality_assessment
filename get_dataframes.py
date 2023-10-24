1e2f32688ffab76de4e9cf5799484b46afabf8d6
Tretyakov Anton <Tretyakov.AA@dns-shop.ru>
2023-10-24 16:07:47 +1000

    Исправление функции get_data(): Более универсальное использование с помощью словаря dict_of_dataframes

--- get_dataframes.py
+++ get_dataframes.py
@@ -1,35 +1,46 @@
 import pandas as pd
 import os
+from datetime import date
 
 
 def get_data():
     os.chdir('csv')  # заходим в папку csv
-    if not 'pricing_types.csv' in os.listdir():  # проверка наличия файлов в папке csv
-        os.chdir('..')  # выходим из папки cs
+    if not os.listdir():  # проверка наличия файлов в папке csv
         print(
             'Ошибка: отсутствуют файлы в "./csv", попробуйте изменить параметр rewrite = True в обращении к функции csv_execute()')
         return False
-
-    pricing_types_df = pd.read_csv('pricing_types.csv')
-    products_reference_df = pd.read_csv('products_reference.csv')
-    reprices_errors_log_df = pd.read_csv('reprices_errors_log.csv')
-    reprices_log_df = pd.read_csv('reprices_log.csv')
-    write_date_df = pd.read_csv('write_date.csv')
+    dict_of_dataframes = {}
+    ''' Создание словаря датафреймов '''
+    for dataframe in os.listdir():
+        dict_of_dataframes[dataframe[0:-4] + '_df'] = pd.read_csv(dataframe)
     os.chdir('..')  # выходим из папки csv
 
-    # преобразуем типы данных
+
+    ''' Преобразование типов данных, настраивается пользователем '''
+    reprices_log_df = dict_of_dataframes['reprices_log_df']
+    reprices_errors_log_df = dict_of_dataframes['reprices_errors_log_df']
+
+    # изменение типа данных
     reprices_log_df['date_reprice'] = pd.to_datetime(reprices_log_df['date_reprice'])
     reprices_errors_log_df['date_error'] = pd.to_datetime(reprices_errors_log_df['date_error'])
 
     reprices_log_df.rename(columns={'date_reprice': 'date'}, inplace=True)
     reprices_errors_log_df.rename(columns={'date_error': 'date'}, inplace=True)
 
-    date_range_ser = pd.date_range(start=min(reprices_log_df['date'] - pd.DateOffset(months=1)),
-                                   end=max(reprices_log_df['date'] + pd.DateOffset(months=1)))
+    # загрузка измененного датафрейма в словарь
+    dict_of_dataframes['reprices_log_df'] = reprices_log_df
+    dict_of_dataframes['reprices_errors_log_df'] = reprices_errors_log_df
+
+    ''' Создание календаря '''
+    date_range_ser = pd.date_range(start=date(2023,1,1),
+                                   end=date(2024,1,1))
     calendar_df = pd.DataFrame({'date': date_range_ser,
                                 'week_day': date_range_ser.weekday})
+    dict_of_dataframes['calendar_df'] = calendar_df
 
     #                            )
     # for elem in reprices_log_df['date_reprice']:
     #     print(type(elem))
-    return pricing_types_df, products_reference_df, reprices_errors_log_df, reprices_log_df, calendar_df, write_date_df
+    return dict_of_dataframes
+
+data = get_data()
